2db529342674530673ee38627ade3ed6819565ce
Vibhishan Ranga <vibhishan.ranga1410@gmail.com>
2025-04-03 19:41:21 +0100

    removed comments
    Signed-off-by: Vibhishan Ranga <vibhishan.ranga1410@gmail.com>

--- src/components/Word.jsx
+++ src/components/Word.jsx
@@ -1,14 +1,10 @@
 import React from "react";
 
 export default function Word({ actualWord, playedLetters }) {
-  console.log(actualWord);
-  console.log(typeof actualWord);
-  console.log(playedLetters);
   return (
     <div>
       {[...actualWord].map((letter, index) => (
         <React.Fragment key={index}>
-          {console.log(letter)}
           {playedLetters.has(letter) ? (
             <span key={index}>{letter}</span>
           ) : (
